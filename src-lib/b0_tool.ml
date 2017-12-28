b0-system/b0

commit e9fc33a689104ca2a1a7ad867bd8155d7ecfb656

OS.{Env,Cmd} API works.

src-lib/b0_tool.ml
@@ -20,7 +20,7 @@ type t =
     unit : B0_unit.t option; }
 
 let v ?(internal = tmp_vars) ?(env_vars = []) name =
-  match B0_os.Cmd.tool_is_path name with
+  match B0_os.Cmd.exe_is_path name with
   | false -> invalid_arg (B0_string.strf "%S: not a tool name" name)
   | true -> { name = B0_fpath.v name; env_vars; internal; unit = None }
 

src-b0/b0b_cli.ml
@@ -348,7 +348,7 @@ let show_variant ?(ext = fun _ _ -> ()) =
 
 (* Editing *)
 
-let edit_files fs = match OS.Env.var "EDITOR" with
+let edit_files fs = match OS.Env.find "EDITOR" with
 | None -> R.error_msg "EDITOR environment variable undefined."
 | Some editor ->
     Cmd.of_string editor
@@ -356,23 +356,23 @@ let edit_files fs = match OS.Env.var "EDITOR" with
     >>= function
     | false -> R.error_msgf "Editor %a not in search path" Cmd.pp editor
     | true ->
-        OS.Cmd.(run_status Cmd.(editor %% of_values p fs)) >>= function
+        OS.Cmd.run_status Cmd.(editor %% of_values p fs) >>= function
         | `Exited n | `Signaled n -> Ok n
 
 let find_pager ~don't = match don't with
 | true -> Ok None
 | false ->
-    match OS.Env.var "TERM" with
+    match OS.Env.find "TERM" with
     | Some "dumb" | None -> Ok None
     | _ ->
         let cmds = ["less"; "more"] in
-        let cmds = match OS.Env.var "PAGER" with
+        let cmds = match OS.Env.find "PAGER" with
         | None -> cmds | Some c -> c :: cmds
         in
         let rec loop = function
         | [] -> Ok None
         | cmd :: cmds ->
-            OS.Cmd.which (Cmd.v cmd) >>= function
+            OS.Cmd.find_tool (Cmd.v cmd) >>= function
             | Some _ as v  -> Ok v
             | None -> loop cmds
         in

src-b0/b0b_run.ml
@@ -66,10 +66,9 @@ let run_exec noenv v o exec args =
       | false -> Variant.Scheme.direct_env d () >>| fun env -> Env.env env aim
       in
       env >>= fun env ->
-      let exec = Fpath.to_string exec in
-      let env = Array.of_list @@ OS.Env.to_assignments env in
-      let args = Array.of_list (exec :: args) in
-      OS.Cmd.execve_raw exec args ~env >>= fun () -> assert false
+      let env = OS.Env.to_assignments env in
+      let argv = Cmd.of_list (Fpath.to_string exec :: args) in
+      OS.Cmd.execv ~env exec argv >>= fun () -> assert false
 
 (* Action *)
 

src-care/b0_docker.ml
@@ -13,10 +13,10 @@ type cmd = { cmd : Cmd.t; }
 let cmd d = d.cmd
 let get () =
   (* TODO abstract this pattern, see `B0_opam` *)
-  let docker = OS.Env.opt_var "B0_DOCKER" ~absent:"docker" in
-  match OS.Cmd.which_raw docker with
-  | None -> R.error_msgf "docker: not found in PATH (as %s)" docker
-  | Some docker -> Ok { cmd = Cmd.v docker }
+  let absent = Cmd.v "docker" in
+  let docker = OS.Env.get_value "B0_DOCKER" Conv.tool ~absent in
+  OS.Cmd.resolve docker >>= fun cmd -> Ok { cmd }
+
 
 (* Managing images. *)
 
@@ -41,12 +41,12 @@ let image_create d i b = match b.context with
          p b.dockerfile % c)
 
 let image_exists d i =
-  OS.Cmd.(run_out Cmd.(d.cmd % "images" % "-q" % i) |> to_string)
+  OS.Cmd.run_out Cmd.(d.cmd % "images" % "-q" % i)
   >>| fun id -> id <> ""
 
 let image_delete d i = image_exists d i >>= function
 | false -> Ok ()
-| true -> OS.Cmd.(run_out Cmd.(d.cmd % "rmi" % i) |> to_null)
+| true -> OS.Cmd.(run ~stdout:out_null Cmd.(d.cmd % "rmi" % i))
 
 (* Managing containers. *)
 
@@ -54,7 +54,7 @@ type container = string
 
 let container_exists d c =
   let c = strf "name=%s" c in
-  OS.Cmd.(run_out Cmd.(d.cmd % "ps" % "-a" % "-q" % "-f" % c) |> to_string)
+  OS.Cmd.run_out Cmd.(d.cmd % "ps" % "-a" % "-q" % "-f" % c)
   >>| fun id -> id <> ""
 
 let container_create d ?workdir:w ?(binds = []) i c =

src-care/b0_opam.ml
@@ -12,10 +12,9 @@ type pkg = string
 type cmd = { cmd : Cmd.t; }
 
 let get () =
-  let opam = OS.Env.opt_var "B0_OPAM" ~absent:"opam" in
-  match OS.Cmd.which_raw opam with
-  | None -> R.error_msgf "opam: not found in PATH (as %s)" opam
-  | Some opam -> Ok { cmd = Cmd.v opam }
+  let absent = Cmd.v "opam" in
+  let opam = OS.Env.get_value "B0_OPAM" Conv.tool ~absent in
+  OS.Cmd.resolve opam >>= fun cmd -> Ok { cmd }
 
 let cmd opam = opam.cmd
 let env opam ~switch =
@@ -36,7 +35,7 @@ let env opam ~switch =
     | Failure e -> R.error_msg e
   in
   let cmd = Cmd.(opam.cmd % "env" % "--sexp" % "--switch" % switch) in
-  OS.Cmd.(run_out cmd |> to_string)
+  OS.Cmd.run_out cmd
   >>= fun out -> Sexp.of_string ~src:(Sexp.File OS.File.dash) out
   >>= fun se -> parse_env_sexp (Sexp.get_list se)
 

src-care/b0_ssh.ml
@@ -9,17 +9,17 @@ open B0
 type host = string
 
 let get_ssh () =
-  (* TODO abstract this pattern, see `B0_opam` *)
-  let ssh = OS.Env.opt_var "B0_SSH" ~absent:"ssh" in
-  match OS.Cmd.which_raw ssh with
-  | None -> R.error_msgf "ssh: not found in PATH (as %s)" ssh
-  | Some ssh -> Ok (Cmd.v ssh)
+  (* TODO abstract this pattern, see `B0_opam`.
+     Also review the which stuff again there's a lot of conversion
+     going on here. *)
+  let absent = Cmd.v "ssh" in
+  let ssh = OS.Env.get_value "B0_SSH" Conv.tool ~absent in
+  OS.Cmd.resolve ssh
 
 let get_rsync () =
-  let ssh = OS.Env.opt_var "B0_RSYNC" ~absent:"rsync" in
-  match OS.Cmd.which_raw ssh with
-  | None -> R.error_msgf "rsync: not found in PATH (as %s)" ssh
-  | Some rsync -> Ok (Cmd.v rsync)
+  let absent = Cmd.v "rsync" in
+  let rsync = OS.Env.get_value "B0_RSYNC" Conv.tool ~absent in
+  OS.Cmd.resolve rsync
 
 let run excludes remote_root host scheme conf cmd =
   let root =

src-care/b0c_os.ml
@@ -24,7 +24,7 @@ let warn_if_error e ~use:v =
 
 let uname env aim opts =
   tool env aim "uname" >>= fun uname ->
-  OS.Cmd.(run_out Cmd.(uname %% opts) |> to_string)
+  OS.Cmd.run_out Cmd.(uname %% opts)
 
 (* Architecture *)
 
@@ -110,7 +110,7 @@ let name =
 
 let android_version =
   let cmd = Cmd.(v "getprop" % "ro.build.version.release") in
-  let version = lazy (OS.Cmd.(run_out ~err:OS.File.null cmd |> to_string)) in
+  let version = lazy OS.Cmd.(run_out ~stderr:(`Stdo out_null) cmd) in
   fun () -> match Lazy.force version with
   | Error _ -> None
   | Ok v -> Some v
@@ -180,7 +180,7 @@ let distribution_linux os = match android_version () with
     | None -> os
     | Some v -> v
 
-let distribution_macos os =
+let distribution_macos os =  (* FIXME lookup in right env *)
   OS.Cmd.exists (Cmd.v "brew") >>= function
   | true -> Ok "homebrew"
   | false ->
@@ -214,11 +214,9 @@ let version_linux () = match android_version () with
     | None -> Ok "unknown"
     | Some v -> Ok v
 
-let version_macos () =
-  OS.Cmd.(run_out Cmd.(v "sw_vers" % "-productVersion") |> to_string)
-
+let version_macos () = OS.Cmd.run_out Cmd.(v "sw_vers" % "-productVersion")
 let version_windows () =
-  let wmic opts = OS.Cmd.(run_out Cmd.(v "wmic" %% opts) |> to_string) in
+  let wmic opts = OS.Cmd.run_out Cmd.(v "wmic" %% opts) in
   wmic Cmd.(v "os" % "get" % "Version" % "/value") >>= fun kv ->
   match String.cut ~sep:"=" kv with
   | None -> R.error_msgf "Could not parse wmic value from %S" kv

src-driver/b0d_driver.ml
@@ -158,8 +158,9 @@ let exec_instance d cli descr =
      Sys.argv and replace it by this one. The problem with this
      is the intepretation of relative files paths andinteraction
      with -C. *)
+  let argv = Cmd.of_list @@ Array.to_list @@ Sys.argv in
   OS.Dir.set_current (B0d_root_descr.dir descr)
-  >>= fun () -> OS.Cmd.execv_raw (Fpath.to_string bin) Sys.argv
+  >>= fun () -> OS.Cmd.execv bin argv
   >>| fun () -> (* should not happen *) B0d_cli.exit_driver_setup_err
 
 (* Wrapping driver commands *)

commit 1ff65df882dbca50d73a81f02c1be1972c87ea2d

Fix regression in Tool.v introduced by e9fc33a6.

src-lib/b0_tool.ml
@@ -21,8 +21,8 @@ type t =
 
 let v ?(internal = tmp_vars) ?(env_vars = []) name =
   match B0_os.Cmd.exe_is_path name with
-  | false -> invalid_arg (B0_string.strf "%S: not a tool name" name)
-  | true -> { name = B0_fpath.v name; env_vars; internal; unit = None }
+  | true -> invalid_arg (B0_string.strf "%S: not a tool name" name)
+  | false -> { name = B0_fpath.v name; env_vars; internal; unit = None }
 
 let of_file ?(internal = tmp_vars) ?(env_vars = []) name =
   { name; env_vars; internal; unit = None }