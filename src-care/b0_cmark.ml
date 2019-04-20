9471bdf6a8b0a95ba05a8d8ad323b11170b4adca
Daniel Bünzli <daniel.buenzli@erratique.ch>
2019-04-20 19:26:46 +0200

    Add B0_cmark.

---
+++ src-care/b0_cmark.ml
@@ -0,0 +1,51 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 The b0 programmers. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
+open B0_std
+open B00
+
+let tool = Tool.by_name "cmark"
+let cmd
+    ?(validate_utf_8 = true) ?(format = "html") ?args:(more_args = Cmd.empty) m
+    ~mds ~o
+  =
+  let cmark = Memo.tool m tool in
+  let validate = Cmd.if' validate_utf_8 (Cmd.arg "--validate-utf8") in
+  Memo.spawn m ~reads:mds ~writes:[o] ~stdout:(`File o) @@
+  cmark Cmd.(arg "--to" % format %% validate %% more_args %% shield (paths mds))
+
+let write_page
+    ?(generator = "") ?(lang = "") ?(scripts = []) ?(styles = []) ?(title = "")
+    m ~frag ~o
+  =
+  let open B0_web.Htmlg in
+  Memo.read m frag @@ fun contents ->
+  let title = if title = "" then Fpath.basename ~no_ext:true o else title in
+  let stamp =
+    let data = generator :: lang :: title :: List.rev_append styles scripts in
+    Hash.to_bytes (Memo.hash_string m (String.concat "" data))
+  in
+  Memo.write m ~stamp ~reads:[frag] o @@ fun () ->
+  let open B0_web.Htmlg in
+  let body = El.body [El.raw contents] in
+  let page = El.basic_page ~generator ~lang ~scripts ~styles ~title body in
+  Ok (El.to_string ~doc_type:true page)
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 The b0 programmers
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
---
+++ src-care/b0_cmark.mli
@@ -0,0 +1,57 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 The b0 programmers. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   %%NAME%% %%VERSION%%
+  ---------------------------------------------------------------------------*)
+
+(** B0 [cmark] support.
+
+    This models the {{:https://github.com/commonmark/cmark}[cmark]}
+    tool. There's not much to model though. *)
+
+open B0_std
+
+(** {1:cmark Cmark} *)
+
+val tool : B00.Tool.t
+(** [tool] is the [cmark] tool. *)
+
+val cmd :
+  ?validate_utf_8:bool -> ?format:string -> ?args:Cmd.t -> B00.Memo.t ->
+  mds:Fpath.t list -> o:Fpath.t -> unit
+(** [cmd m ~format ~validate_utf_8 ~args ~mds ~o] writes to file [o] the
+    result of processing the CommonMark files [mds].
+    {ul
+       {- [format] is the output format. It defaults to ["html"] which
+           outputs a fragment not a full page page (this the [--to]
+           option).}
+       {- [validate_utf_8] is the [--validate-utf8] option it defaults
+          to [true].}
+       {- [args] are additional command line arguments you may want to
+          pass to [cmark].}} *)
+
+val write_page :
+  ?generator:string -> ?lang:string -> ?scripts:string list ->
+  ?styles:string list -> ?title:string -> B00.Memo.t ->
+  frag:Fpath.t -> o:Fpath.t -> unit
+(** [write_page m ~title ~frag ~o] writes to [o] a full HTML document
+    whose {!B0_web.Htmlg.El.body} contains the contents of file [frag]
+    (inserted using {!B0_web.Htmlg.raw}). [title] defaults to the
+    basename of [o] without its extension, for the other arguments
+    and more information see {!B0_web.Htmlg.basic_page}. *)
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 The b0 programmers
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
