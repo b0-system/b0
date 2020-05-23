1979ad88c01e586ead81db98b327a9ff01722bde
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-05-23 23:16:55 +0200

    B00_std.Result: Use Stdlib.Result to implement it.

--- src-b00/std/b00_std.ml
+++ src-b00/std/b00_std.ml
@@ -517,47 +517,13 @@ end
 
 module Result = struct
 
-  (* Results *)
-
-  type ('a, 'e) t = ('a, 'e) result = Ok of 'a | Error of 'e
-  let ok v = Ok v
-  let error e = Error e
-  let value r ~default = match r with Ok v -> v | Error _ -> default
-  let get_ok = function Ok v -> v | Error _ -> invalid_arg "result is Error _"
-  let get_error = function Error e -> e | Ok _ -> invalid_arg "result is Ok _"
-  let bind r f = match r with Ok v -> f v | Error _ as e -> e
-  let join = function Ok r -> r | Error _ as e -> e
-  let map f = function Ok v -> Ok (f v) | Error _ as e -> e
-  let map_error f = function Error e -> Error (f e) | Ok _ as v -> v
-  let fold ~ok ~error = function Ok v -> ok v | Error e -> error e
-  let iter f = function Ok v -> f v | Error _ -> ()
-  let iter_error f = function Error e -> f e | Ok _ -> ()
-
-  (* Predicates and comparisons *)
-
-  let is_ok = function Ok _ -> true | Error _ -> false
-  let is_error = function Error _ -> true | Ok _ -> false
-  let equal ~ok ~error r0 r1 = match r0, r1 with
-  | Ok v0, Ok v1 -> ok v0 v1
-  | Error e0, Error e1 -> error e0 e1
-  | _, _ -> false
-
-  let compare ~ok ~error r0 r1 = match r0, r1 with
-  | Ok v0, Ok v1 -> ok v0 v1
-  | Error e0, Error e1 -> error e0 e1
-  | Ok _, Error _ -> -1
-  | Error _, Ok _ -> 1
+  include Stdlib.Result
 
   (* Interacting with Stdlib exceptions *)
 
   let to_failure = function Ok v -> v | Error e -> failwith e
   let catch_failure f = try Ok (f ()) with Failure e -> Error e
   let catch_sys_error f = try Ok (f ()) with Sys_error e -> Error e
-
-  (* Converting *)
-
-  let to_option = function Ok v -> Some v | Error _ -> None
-  let to_list = function Ok v -> [v] | Error _ -> []
 end
 
 (* Characters *)
