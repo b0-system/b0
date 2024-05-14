b0-system/b0

commit 3a8be150a935ff0486e3d250f5416be53ff26f9d

Rework command lookups and `Os.Cmd.execv` interface.
Allows to use `B0_env.get_cmd env` with `Os.Cmd.get`. The new pattern
for abtracting tool lookups in `B0_http`, `B0_opam` etc. is to use an
optional argument: `?search:Cmd.tool_search` and perform lookups with
`Os.Cmd.{get,find} ?search`.

src/file/b0_driver.ml
@@ -399,8 +399,12 @@ let with_b0_file ~driver cmd =
     let* b0_file = Conf.get_b0_file conf in
     Log.if_error' ~use:Exit.b0_file_error @@
     let* exe = compile_b0_file conf ~driver ~feedback:true b0_file in
-    let argv = Cmd.list (Array.to_list Sys.argv) in
-    Ok (Os.Exit.exec exe argv)
+    let exe = Fpath.to_string exe in
+    let cmd = match Array.to_list Sys.argv with
+    | [] -> Cmd.arg exe
+    | _ :: args -> Cmd.list (exe :: args)
+    in
+    Ok (Os.Exit.exec cmd)
   in
   Cmdliner.Term.(const run $ Cli.conf $ cmd)
 
@@ -418,8 +422,12 @@ let with_b0_file_if_any ~driver cmd =
             has_failed_b0_file := true;
             cmd conf
         | Ok exe ->
-            let argv = Cmd.list (Array.to_list Sys.argv) in
-            Os.Exit.exec exe argv
+            let exe = Fpath.to_string exe in
+            let cmd = match Array.to_list Sys.argv with
+            | [] -> Cmd.arg exe
+            | _ :: args -> Cmd.list (exe :: args)
+            in
+            Os.Exit.exec cmd
   in
   Cmdliner.Term.(const run $ Cli.conf $ cmd)
 

B0.ml
@@ -36,7 +36,7 @@ let b0_kit_lib =
 (* The b0 tool *)
 
 let bootstrap_env env unit =
-  let boot_root = (* FIXME need to access the root of the build  *)
+  let boot_root = (* TODO b0: need to access the root of the build  *)
     Fpath.to_string (B0_env.in_unit_dir env unit ~/"..")
   in
   B0_env.build_env env
@@ -163,20 +163,19 @@ let default =
 
 let strap =
   B0_action.make "strap" ~doc:"Run boot/strap" @@
-  B0_action.exec_file' ~/"boot/strap"
+  B0_action.exec_file (Cmd.tool "boot/strap")
 
 let bowl =
   let doc = "Run built b0 in the bowl directory" in
   B0_action.make "bowl" ~units:[b0] ~doc @@
   fun _ env ~args ->
-  match B0_env.unit_exe_file env b0 with (* FIXME b0 error struct *)
+  match B0_env.unit_exe_file_cmd env b0 with (* TODO b0: error struct *)
   | Error e -> Log.err (fun m -> m "%s" e); B0_cli.Exit.some_error
-  | Ok exec ->
+  | Ok b0_exe ->
       let cwd = B0_env.in_scope_dir env ~/"bowl" in
       let env = bootstrap_env env b0 |> Result.get_ok in
       let env = Os.Env.to_assignments env in
-      (* FIXME b0 B0_env.exec_file allow to choose name of args ? *)
-      Os.Exit.exec ~cwd ~env exec Cmd.(path exec %% args)
+      Os.Exit.exec ~cwd ~env Cmd.(b0_exe %% args)
 
 let vendor_htmlit =
   let doc = "Vendor Htmlit and expose it as B0_html" in

doc/action.mld
@@ -55,12 +55,12 @@ let mycmd =
 
 Even though you should rewrite all these scripts as OCaml actions a
 direct short cut for the above boilerplate is provided.  Use
-{!B0_action.exec_file'}:
+{!B0_action.exec_file}:
 
 {[
 let mycmd =
   B0_action.make "mycmd" ~doc:"Run mycmd" @@
-  B0_action.exec_file' ~/"scripts/mycmd"
+  B0_action.exec_file ~/"scripts/mycmd"
 ]}
 
 {1:howto Howto}

src/file/b0_action.ml
@@ -68,21 +68,21 @@ let pp ppf v =
 
 (* Script and tool execution *)
 
-let exec_file ?env:e ?cwd env file ~args =
+let exec_file ?env:e ?cwd cmd t env ~args =
   let scope_dir = B0_env.scope_dir env in
   let cwd = Option.value ~default:scope_dir cwd in
-  let file = Fpath.(scope_dir // file) in
-  Os.Exit.exec ?env:e ~cwd file Cmd.(path file %% args)
+  match Cmd.get_tool cmd with
+  | Error e -> exit_some_error e
+  | Ok file ->
+      let file = Fpath.(scope_dir // file) in
+      Os.Exit.exec ?env:e ~cwd Cmd.(set_tool file cmd %% args)
 
-let exec_file' ?env:e ?cwd file =
-  fun _ env ~args -> exec_file ?env:e ?cwd env file ~args
-
-let exec_tool ?env:e ?cwd env tool ~args = match Os.Cmd.get_tool tool with
+let exec_cmd ?env:e ?cwd env cmd = match B0_env.get_cmd env cmd with
 | Error e -> exit_some_error e
-| Ok exe ->
+| Ok cmd ->
     let scope_dir = B0_env.scope_dir env in
     let cwd = Option.value ~default:scope_dir cwd in
-    Os.Exit.exec ?env:e ~cwd exe Cmd.(path exe %% args)
+    Os.Exit.exec ?env:e ~cwd cmd
 
 (* Command line interaction. *)
 

src/file/b0_action.mli
@@ -72,31 +72,24 @@ val exit_of_result' : (Os.Exit.t, string) result -> Os.Exit.t
 (** {1:script Action functions for script execution} *)
 
 val exec_file :
-  ?env:Os.Env.assignments -> ?cwd:Fpath.t ->
-  B0_env.t -> Fpath.t -> args:Cmd.t -> Os.Exit.t
-(** [exec_file env file ~args] executes [file] with arguments [args].
-    The {{!B0_env.scope_dir}scope directory} is used
-    as the default [cwd] and to resolve [file] if it is relative. *)
-
-val exec_file' :
-  ?env:Os.Env.assignments -> ?cwd:Fpath.t -> Fpath.t -> func
-(** [exec_file'] is {!exec_file} twisted a bit differently so
-    that if you don't need to tweak arguments or lookup the environment
-    you can simply write:
+  ?env:Os.Env.assignments -> ?cwd:Fpath.t -> Cmd.t -> func
+(** [exec_file env cmd] executes [cmd] using
+    the {{!B0_env.scope_dir}scope directory} is used
+    as the default [cwd] and to resolve [file] if it is relative. This
+    signature is twisted so that you can simply write:
     {[
 let myscript =
   B0_action.make "myscript" @@
   B0_action.exec_file' ~/"scripts/myscript"
-    ]} *)
+    ]}
+*)
 
-val exec_tool :
-  ?env:Os.Env.assignments -> ?cwd:Fpath.t ->
-  B0_env.t -> Cmd.tool -> args:Cmd.t -> Os.Exit.t
-(** [exec_tool tool e args] executes the tool [exe] with arguments
-    [cmd] The {{!B0_env.scope_dir}scope directory} is used as the default
-    [cwd]. [exe] is looked up using {!B0_std.Os.Cmd.get_tool}, if
-    that fails the error is logged and we exit we and exits with
-    {!B0_cli.Exit.some_error}. *)
+val exec_cmd :
+  ?env:Os.Env.assignments -> ?cwd:Fpath.t -> B0_env.t -> Cmd.t -> Os.Exit.t
+(** [exec_tool tool cmd] executes [cmd]. The {{!B0_env.scope_dir}scope
+    directory} is used as the default [cwd]. The tool of [cmd] is looked up
+    using {!B0_env.get_cmd}, if that fails the error is logged and we exit
+    we and exits with {!B0_cli.Exit.some_error}. *)
 
 (** {1:cli Command line interaction}
 

src/file/b0_def.mli
@@ -143,10 +143,10 @@ module type S = sig
   (** [add v] adds the value [v] to the list of defined values. *)
 
   val fold : (t -> 'a -> 'a) -> 'a -> 'a
-  (** [fold f acc] folds over the list of units. *)
+  (** [fold f acc] folds over the list of defined values. *)
 
   val list : unit -> t list
-  (** [list ()] is the list of units. *)
+  (** [list ()] is the list of defined values. *)
 
   val find : string -> t option
   (** [find name] is the value named [name] (if any). *)

src/file/b0_env.mli
@@ -101,20 +101,16 @@ val build : t -> B0_build.t
 
 (** {1:tool Tool lookup} *)
 
-val get_tool : ?skip_build:bool -> t -> Cmd.tool -> (Cmd.tool, string) result
-(** [get_tool env tool] looks up the file path to of tool [tool] in
-    the environment. If [tool] is defined by a unit in the build and
-    is {!B0_unit.tool_is_user_accessible}, it comes first in the
+val get_cmd : ?skip_build:bool -> t -> Cmd.tool_search
+(** [get_cmd env cmd] is [cmd] with its {!B0_std.Cmd.tool} resolved in
+    the environment. If [cmd]'s tool is defined by a unit in the build
+    and is {!B0_unit.tool_is_user_accessible}, it comes first in the
     search, unless [skip_build] is [true] (defaults to [false]. *)
-
-val get_cmd : ?skip_build:bool -> t -> Cmd.t -> (Cmd.t, string) result
-(** [get_cmd] performs {!get_tool} on the command's tool. *)
 
 val unit_exe_file : t -> B0_unit.t -> (Fpath.t, string) result
 (** [unit_exe_file env u] looks up the {!B0_unit.exe_file}
     of [u] in [env]. This errors if [u] can't be found in the build
     of if [u] has no such key. *)
 
-val unit_cmd : t -> B0_unit.t -> (Cmd.t, string) result
-(** [unit_cmd env u] is like {!unit_exe_file} but returns a command
-    instead. *)
+val unit_exe_file_cmd : t -> B0_unit.t -> (Cmd.t, string) result
+(** [unit_exe_file env u] is [Result.map Cmd.path (unit_exe_file env u)]. *)

src/file/b0_ocaml.ml
@@ -1794,13 +1794,13 @@ let ocaml env use_utop dry_run args =
       m "Did build: @[%a@]"
         Fmt.(iter B0_unit.Set.iter ~sep:sp B0_unit.pp_name) units;
   end;
-  let top = if use_utop then Fpath.v "utop" else Fpath.v "ocaml" in
-  let* exe = B0_env.get_tool env top in
+  let top = Cmd.tool (if use_utop then "utop" else "ocaml") in
+  let* exe = B0_env.get_cmd env top in
   let* load_args = byte_code_build_load_args b (B0_unit.Set.elements units) in
   let args = Cmd.of_list Fun.id args in
-  let top = Cmd.(path exe %% load_args %% args) in
+  let top = Cmd.(exe %% load_args %% args) in
   match dry_run with
-  | false -> Ok (Os.Exit.exec exe top)
+  | false -> Ok (Os.Exit.exec top)
   | true ->
       Log.app (fun m -> m "%s" (Cmd.to_string top));
       Ok B0_cli.Exit.ok

commit 558ad0a850bad349c1787bfa345ae0483f4e129b

Restore driver's executable execv name as b0.
Regression introduce by the 3a8be150a9 refactoring.

src/file/b0_driver.ml
@@ -403,7 +403,7 @@ let with_b0_file ~driver cmd =
     | [] -> Cmd.arg exe
     | _ :: args -> Cmd.list (exe :: args)
     in
-    Ok (Os.Exit.exec cmd)
+    Ok (Os.Exit.exec ~argv0:"b0" cmd)
   in
   Cmdliner.Term.(const run $ Cli.conf $ cmd)
 
@@ -426,7 +426,7 @@ let with_b0_file_if_any ~driver cmd =
             | [] -> Cmd.arg exe
             | _ :: args -> Cmd.list (exe :: args)
             in
-            Os.Exit.exec cmd
+            Os.Exit.exec ~argv0:"b0" cmd
   in
   Cmdliner.Term.(const run $ Cli.conf $ cmd)
 