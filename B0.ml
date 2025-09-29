1434c7ca873ff7df36fde8981df47ad308ace6ef
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-09-29 13:39:38 +0200

    Preparing for release

--- B0.ml
+++ B0.ml
@@ -177,7 +177,7 @@ let default =
       "ocamlfind", {|build|};
       "ocamlbuild", {|build|};
       "topkg", {|build & >= "1.1.0"|};
-      "cmdliner", {|>= "1.3.0"|}; ]
+      "cmdliner", {|>= "2.0.0"|}; ]
     |> B0_meta.tag B0_opam.tag
     |> B0_meta.tag B0_release.tag
   in
